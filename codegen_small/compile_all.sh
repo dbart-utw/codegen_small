7f40d93284ff160dbef238a4ce21242066bcc724
Duncan Bart <d.bart@utwente.nl>
2024-09-20 13:06:13 +0200

    compiled for ARM

--- codegen_small/compile_all.sh
+++ codegen_small/compile_all.sh
@@ -13,7 +13,7 @@ do
                     # If ./main does not exists in this directory, compile
                     if [ ! -f "$dir4/main" ]; then
                         echo "Compiling $dir4"
-                        cd "$dir4" && gcc -g -o main -O3 main.c && cd ../../../../../
+                        cd "$dir4" && arm-linux-gnueabi-gcc -g -o main -O3 main.c && cd ../../../../../
                     fi
                 done
             done
